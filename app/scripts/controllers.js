0aa6257e59ea20d07b1016a1ae3d773ed81b1e1b
Wang Zhe <headkarl@163.com>
2015-07-17 18:01:43 +0800

    2015.7.17
    ui

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -20,6 +20,86 @@ helloworldApp.controller('mainCtrl', function ($scope) {
 
 //ui 展示页控制器
 helloworldApp.controller('uiCtrl', function ($scope) {
+    var lineChart = $("#lineChart").get(0).getContext("2d");
+    var lineData = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+    var myLineChart = new Chart(lineChart).Line(lineData);
+
+    var radarChart = $("#radarChart").get(0).getContext("2d");
+    var radarData = {
+        labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 90, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 96, 27, 100]
+            }
+        ]
+    };
+    var myRadarChart = new Chart(radarChart).Radar(radarData);
+
+    var barChart = $("#barChart").get(0).getContext("2d");
+    var barData = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+    var myBarChart = new Chart(barChart).Bar(barData);
+
 
   });
 
@@ -84,12 +164,6 @@ helloworldApp.controller('nglabCtrl', function ($scope,$rootScope,vmInfos) {
         $rootScope.showHelper = true;
     };
 
-
-    // $scope.queryVmInfos = function(){
-    //   $scope.vm_infos = vmInfos.query().vm_infos;
-    // };
-
-    // $scope.queryVmInfos();
   	$scope.person = {
         firstName: "John",
         lastName: "Doe"
